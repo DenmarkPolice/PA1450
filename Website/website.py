6aa6ddd7a3e2608178b31a4851d0e6a65e92ba01
JWattwil <wattwiljacob@gmail.com>
2020-05-13 12:55:31 +0200

    "Multi-dropdown"

--- Website/website.py
+++ Website/website.py
@@ -2,6 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from app import app
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -16,23 +17,24 @@ website.layout = html.Div(children = [
             html.P("Temperatur")
         ])
     ]),
-
+    html.Label('Välj en stad'),
     dcc.Dropdown(
         id='dropdown2',
         options=[
             {'label': i, 'value' : i} for i in ['KSD', 'KNA', 'GTB']],
-            value = 'KSD'
+            value = 'KSD',
+            multi=True,
     ),
     html.Div(id='dd-output-container'),
     html.Br()
 ])
 
 
-# @app.callback(
-#     dash.dependencies.Output('dd-output-container', 'children'),
-#     [dash.dependencies.Input('dropdown2', 'value')])
-# def update_output(value):
-#     return 'You have selected "{}"'.format(value)
+@app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('dropdown2', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
 
 
 if __name__ == '__main__':

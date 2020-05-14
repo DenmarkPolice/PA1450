663e468b67e57dc3ecae17210084baf8c3a2fac5
JWattwil <wattwiljacob@gmail.com>
2020-05-14 11:31:16 +0200

    Changed title of site

--- testapp.py
+++ testapp.py
@@ -18,7 +18,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Fuck SMHI'),
+    html.H1(children='Historical weather data provided by SMHI'),
 
 
     dcc.Dropdown(

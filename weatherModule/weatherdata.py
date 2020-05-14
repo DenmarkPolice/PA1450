1f11a4d00ce9d716acb2677bcab3977a5aa8ec43
Hoozt <61567735+Hoozt@users.noreply.github.com>
2020-05-14 09:30:45 +0200

    Update weatherdata.py

--- weatherModule/weatherdata.py
+++ weatherModule/weatherdata.py
@@ -14,12 +14,12 @@ class weatherdata:
 
         for filename in all_files:
             df_list.append(pd.read_csv(filename, sep = ";"))
+            
 
         for df in df_list:
             print(df.info())
             print (df.head())
-            del df[df.columns[4]]
-            del df[df.columns[5]]
+            df.drop(columns = ["Tidsutsnitt", "Kvalitet"])
             
 
         self.data_frame = df_list

3ed63654599ae527f3bc53e440a771c7b849f2f3
Adam Svensson <58476156+adamzki99@users.noreply.github.com>
2020-05-14 09:47:31 +0200

    Update weatherdata.py

--- weatherModule/weatherdata.py
+++ weatherModule/weatherdata.py
@@ -5,7 +5,7 @@ import glob
 class weatherdata:
 
     def __init__(self):
-        self.data_frame = pd.DataFrame()
+        self.data_frame
 
     def import_to_dataset(self, filePath):
         all_files = glob.glob(filePath + "/*.csv")
@@ -13,14 +13,7 @@ class weatherdata:
         df_list = []
 
         for filename in all_files:
-            df_list.append(pd.read_csv(filename, sep = ";"))
-
-        for df in df_list:
-            print(df.info())
-            print (df.head())
-            del df[df.columns[4]]
-            del df[df.columns[5]]
-            
+            df_list.append(pd.read_csv(filename, sep = ";"))         
 
         self.data_frame = df_list
 

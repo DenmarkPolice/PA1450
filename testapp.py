6e831b182b2c4edafb43861d462a98d951492c3b
JWattwil <wattwiljacob@gmail.com>
2020-05-15 10:31:20 +0200

    Completely refactored update_graf function

--- testapp.py
+++ testapp.py
@@ -117,40 +117,28 @@ app.layout = html.Div(children=[
 dash.dependencies.Input('date-pick-range', 'end_date'), dash.dependencies.Input('attribute-dropdown', 'value'), dash.dependencies.Input('year-dropdown', 'value')])
 def update_graf(start_date, end_date, atr_value, year_value):
 
-    #Checks so that all of the function parameters are not null, only then a graph can be made. 
-    if start_date is not None and end_date is not None and atr_value is not None:
-        #This gets us all of the data frames concerning the given dates
+    #Find out whether the user has chosen an interval or an entire year
+    if atr_value is not None and start_date is not None and end_date is not None:
         data_frames = data.get_ranged_df(str(start_date), str(end_date))
-
-        #Find which attribute is selected by the user
-        counter = 0
-        for data_frame in data_frames:
-            if data_frame.columns[2] == atr_value: 
-                break #Right graph found, break out. 
-            counter += 1 #Otherwise increment counter and keep looking. 
-
-        
-        fig = px.line(data_frames[counter], x = data_frames[counter].columns[1], y = data_frames[counter].columns[2]) #Make the graph
-        text = "" #text is only for errors, but since it is an output we have to return something
-        return fig, text
     elif atr_value is not None and year_value is not None:
         start_date = str(year_value) + '-01-01' #The first of January the given year
         end_date = str(year_value) + '-12-12' #The last of December the given year
         data_frames = data.get_ranged_df(str(start_date), str(end_date))
-
-        #Find which attribute is selected by the user
-        counter = 0
-        for data_frame in data_frames:
-            if data_frame.columns[2] == atr_value: 
-                break #Right graph found, break out. 
-            counter += 1 #Otherwise increment counter and keep looking. 
-        
-        fig = px.line(data_frames[counter], x = data_frames[counter].columns[1], y = data_frames[counter].columns[2]) #Make the graph
-        text = "" #text is only for errors, but since it is an output we have to return something
-        return fig, text
     else:
         #If all of the parameters are not null, we can't create a graph, so we return an empty graph and an error message. 
         return {'data': []}, "Please choose both an attribute and some form of time interval"
+
+    #Find which attribute is selected by the user
+    frame_num = 0
+    for data_frame in data_frames:
+        if data_frame.columns[2] == atr_value: 
+            break #Right graph found, break out. 
+        frame_num += 1 #Otherwise increment counter and keep looking.     
+
+    fig = px.line(data_frames[frame_num], x = data_frames[frame_num].columns[1], y = data_frames[frame_num].columns[2]) #Make the graph
+    text = "" #text is only for errors, but since it is an output we have to return something
+    return fig, text
+    
 
 
 #Callback that disables the date range picker if a year is selected in the drodown menu.

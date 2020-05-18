ca25947697f536ef41bcd0d7f2a8f2543a73aab6
JWattwil <wattwiljacob@gmail.com>
2020-05-18 10:20:26 +0200

    Trying to make multi attributes work

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+import plotly.graph_objects as go
 from datetime import datetime as dt
 from dash.dependencies import Input, Output, State
 import re
@@ -41,7 +42,7 @@ app.layout = html.Div(children=[
         id='attribute-dropdown',
         options=[{'label':atr, 'value':atr} for atr in attributes],
         placeholder='Pick attributes',
-        multi=False
+        multi=True
     ),
     html.Br(),
 
@@ -85,18 +86,48 @@ def generateGraph(dataframe):
     dictionary[dataframe.columns[0]] = date_and_time
     dictionary[dataframe.columns[2]] = dataframe[dataframe.columns[2]]
 
-    return px.line(dictionary, x = dataframe.columns[0], y = dataframe.columns[2])
+    #return go.Scatter(dictionary, x = dataframe.columns[0], y = dataframe.columns[2], mode='lines')
+    return px.line(dictionary, x = dataframe.columns[0], y = dataframe.columns[2],)
 
 #Displays the graph based on the attribute selected in the dropdown. 
+# @app.callback([dash.dependencies.Output('scatter-chart', 'figure'), dash.dependencies.Output('fig-error', 'children')], [dash.dependencies.Input('date-pick-range', 'start_date'), 
+# dash.dependencies.Input('date-pick-range', 'end_date'), dash.dependencies.Input('attribute-dropdown', 'value'), dash.dependencies.Input('year-dropdown', 'value')])
+# def update_graf(start_date, end_date, atr_value, year_value):
+#     '''Updates the graph based on attributes and/or dates'''
+
+#     #Find out whether the user has chosen an interval or an entire year
+#     if atr_value is not None and start_date is not None and end_date is not None:
+#         data_frames = data.get_ranged_df(str(start_date), str(end_date))
+#     elif atr_value is not None and year_value is not None:
+#         start_date = str(year_value) + '-01-01' #The first of January the given year
+#         end_date = str(year_value) + '-12-12' #The last of December the given year
+#         data_frames = data.get_ranged_df(str(start_date), str(end_date))
+#     else:
+#         #If all of the parameters are not null, we can't create a graph, so we return an empty graph and an error message. 
+#         return {'data': []}, "Please choose both an attribute and some form of time interval"
+
+#     #Find which attribute is selected by the user
+#     frame_num = 0
+#     for data_frame in data_frames:
+#         if data_frame.columns[2] == atr_value: 
+#             break #Right graph found, break out. 
+#         frame_num += 1 #Otherwise increment counter and keep looking.     
+
+#     fig = generateGraph(data_frames[frame_num])
+
+#     #fig = px.line(data_frames[frame_num], x = data_frames[frame_num].columns[1], y = data_frames[frame_num].columns[2]) #Make the graph
+#     text = "" #text is only for errors, but since it is an output we have to return something
+#     return fig, text
+
 @app.callback([dash.dependencies.Output('scatter-chart', 'figure'), dash.dependencies.Output('fig-error', 'children')], [dash.dependencies.Input('date-pick-range', 'start_date'), 
 dash.dependencies.Input('date-pick-range', 'end_date'), dash.dependencies.Input('attribute-dropdown', 'value'), dash.dependencies.Input('year-dropdown', 'value')])
-def update_graf(start_date, end_date, atr_value, year_value):
+def update_graf(start_date, end_date, atr_values, year_value):
     '''Updates the graph based on attributes and/or dates'''
 
     #Find out whether the user has chosen an interval or an entire year
-    if atr_value is not None and start_date is not None and end_date is not None:
+    if atr_values is not None and start_date is not None and end_date is not None:
         data_frames = data.get_ranged_df(str(start_date), str(end_date))
-    elif atr_value is not None and year_value is not None:
+    elif atr_values is not None and year_value is not None:
         start_date = str(year_value) + '-01-01' #The first of January the given year
         end_date = str(year_value) + '-12-12' #The last of December the given year
         data_frames = data.get_ranged_df(str(start_date), str(end_date))
@@ -104,16 +135,21 @@ def update_graf(start_date, end_date, atr_value, year_value):
         #If all of the parameters are not null, we can't create a graph, so we return an empty graph and an error message. 
         return {'data': []}, "Please choose both an attribute and some form of time interval"
 
-    #Find which attribute is selected by the user
-    frame_num = 0
-    for data_frame in data_frames:
-        if data_frame.columns[2] == atr_value: 
-            break #Right graph found, break out. 
-        frame_num += 1 #Otherwise increment counter and keep looking.     
+    #Find which data frames are selected
+    frame_nums = []
+    for attribute in atr_values:
+        frame = 0
+        for data_frame in data_frames:
+            if data_frame.columns[2] == attribute:
+                frame_nums.append(frame)
+                break
+        frame += 1
 
-    fig = generateGraph(data_frames[frame_num])
+    fig = go.Figure()
+    fig.update_layout(showlegend=True)
+    for frame in frame_nums:
+        fig.add_scatter(generateGraph(data_frames[frame]))
 
-    #fig = px.line(data_frames[frame_num], x = data_frames[frame_num].columns[1], y = data_frames[frame_num].columns[2]) #Make the graph
     text = "" #text is only for errors, but since it is an output we have to return something
     return fig, text
 
